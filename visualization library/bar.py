fd01b3a668c23581a58f8031bfb09dfa1860f19d
Snishant709 <117982133+Snishant709@users.noreply.github.com>
2025-10-01 13:19:20 +0000

    visual and bars using matplotlib and seaborn

--- visualization library/bar.py
+++ visualization library/bar.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
+import seaborn as sb
 
 #load the source data
 source=pd.read_excel("/workspaces/Python-Course/numpy/project/indian_employee_data.xlsx")
@@ -24,15 +24,22 @@ print("'X' axis values ",x_value,"\n","'Y' axis values \n",y_value)
 #we will now 1st increase size of the figure
 plt.figure(figsize=(25,8))#giving height , width
 #rotate the label
-plt.xticks(rotation=90,fontsize=10)
+plt.xticks(rotation=90,fontsize=15)
+plt.yticks(rotation=45,ha="right",fontsize=15)
+
 plt.bar(x_value,y_value,width=0.3,color='skyblue', edgecolor='black')#by default width is 1 
 #but the ploting is not perfect the labels are overlapping so below things we can do
 # allign the labels vertically,decrease the size of font ,increase the size of visual
-plt.xlabel("City")
-plt.ylabel("Counts")
+plt.xlabel("City",fontsize=20)
+plt.ylabel("Counts",fontsize=20)
 plt.tight_layout()  # prevents label cutoff
 plt.savefig("bar.png")
 
 
+#by using the sb library we can draw the bar chart more easily then matplotlib
+sb.countplot(x='City',data=df,order=df["City"].value_counts().index,color="skyblue")
+plt.savefig("bar_using_seaborn.png")
+#but it will take all the properties of the plt which is defined above
 
-
+# so in simple word the seaborn library helps to reduce the line of code to draw any visual
+# as compared to the library matplotlib 
